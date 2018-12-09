ebc01502408d7820f366addab36dd6ff4dfb000a
Mohamed Maher <muhamed.elrefaie@gmail.com>
2018-12-09 20:06:19 +0200

    feat: new settings page

---
+++ pages/settings.vue
@@ -0,0 +1,40 @@
+<template lang="pug">
+div
+  h1 Settings
+
+  .grid.is-ultrawide-5.is-widescreen-8.has-space
+    .column
+      form.form
+      .grid.is-tablet-6
+        .column
+          .field
+            label.field-label Full name
+            input.input(type='text' )
+        .column
+          .field
+            label.field-label Email
+            input.input(type='text' )
+        .column
+          .field
+            label.field-label Country
+            input.input(type='text' )
+        .column
+          .field
+            label.field-label City
+            input.input(type='text' )
+
+      button.button.is-primary Save Changes
+
+</template>
+
+
+<script>
+export default {
+  head() {
+    return {
+      title: 'Settings'
+    }
+  },
+  data: () => ({})
+}
+</script>
