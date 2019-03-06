9b14616985e00b6768be3bc1301101fc51968147
Mohamed Maher <muhamed.elrefaie@gmail.com>
2019-03-06 13:36:16 +0200

    feat: add notifications to minmal layout

--- layouts/minimal.vue
+++ layouts/minimal.vue
@@ -2,7 +2,14 @@
 .TheApp
   nuxt
 
-
+  notifications(group='global' position="bottom right")
+    template(slot='body', slot-scope='props')
+      .alert(:class="props.item.type")
+        .alert-content
+          .alert-title(v-if="props.item.title") {{props.item.title}}
+          div(v-html='props.item.text' v-if="props.item.text")
+        button.alert-dismiss(@click='props.close')
+          AppIcon(name="close")
 </template>
 
 <style lang="stylus">
